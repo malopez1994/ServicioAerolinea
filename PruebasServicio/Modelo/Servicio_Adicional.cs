152634c26303d5d9ad0c512739c58ba6621b5544
miguel angel lopez pedraza <arcangel.94.es@hotmail.com>
2021-01-23 15:56:37 -0500

    actualizacion

---
+++ PruebasServicio/Modelo/Servicio_Adicional.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Descripción breve de Pasajero
+/// </summary>
+public class Servicio_Adicional
+{
+  public int Id_Servicio { get; set; }
+  public string Tipo_servicio { get; set; }
+   public int Total_maleta { get; set; }
+  public int Id_Viaje  { get; set; }
+}
---
+++ PruebasServicio/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PruebasServicio")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PruebasServicio")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2b80991c-067f-491c-80d8-4fd9b45eaefa")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
